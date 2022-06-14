a91757549f311b8c02213178dc4f463769d90fb2
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2022-06-14 14:29:13 -0500

    Change localhost urls to relative urls

--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,39 +1,38 @@
 <script>
-  import Header from '../components/Header.svelte'
-  import Main from '../components/Main.svelte'
-  import Sidebar from '../components/Sidebar.svelte'
-  import TimeLine from '../components/TimeLine.svelte'
-  import { onMount } from 'svelte'
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+  import { onMount } from "svelte";
 
-  let data = {}
-  const API = "http://localhost:8080/public/api/posts.json"
+  let data = {};
+  const API = "/public/api/posts.json";
 
-  onMount(async()=>{
-    const response = await fetch(API)
+  onMount(async () => {
+    const response = await fetch(API);
     data = await response.json();
-  })
-
+  });
 </script>
 
-<style>
-  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+<Header />
+<Main>
+  <TimeLine posts={data.posts} />
+  <Sidebar {...data.user} />
+</Main>
 
-  :global(body){
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Lato:wght@300;400&display=swap");
+  @import url("https://fonts.googleapis.com/css2?family=Pacifico&display=swap");
+
+  :global(body) {
     background-color: #fafafa;
-    color: rgba(38,38,38,0.7);
-    font-family: 'Lato', sans-serif;
+    color: rgba(38, 38, 38, 0.7);
+    font-family: "Lato", sans-serif;
     margin: 0;
     padding: 0;
   }
-  :global(h1,h2,h3){
-    margin:0;
-    padding:0;
+  :global(h1, h2, h3) {
+    margin: 0;
+    padding: 0;
   }
 </style>
-
-<Header/>
-  <Main>
-    <TimeLine posts={data.posts}/>
-    <Sidebar {...data.user}/>
-  </Main>
